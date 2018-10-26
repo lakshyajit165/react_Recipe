2661747a9bcb26ae1992068b879b287b517bedca
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-27 00:04:38 +0530

    Committing Recipe React App

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
+const API_KEY = "88a4147dedeb535dc7be1b83180ae268";
+
+
+
+class App extends Component {
+
+  state = {
+    recipes: []
+  }
+
+  
+  getRecipe = async(e) =>{
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`);
+    
+    const data = await api_call.json();
+
+    this.setState({
+      recipes: data.recipes
+    });
+
+    console.log(this.state.recipes);
+  }
+
+  componentWillMount = () =>{
+    const json = localStorage.getItem("recipes"); 
+    const recipes = JSON.parse(json);
+    this.setState({recipes: recipes});
+  }
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes",recipes);
+  } 
+  
+  
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title mr-auto"><i className="fa fa-search"></i>Recipe Search</h1>
+          <p className="lead">Search to view to some lipsmacking recipes!</p>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Recipe extends React.Component{
+
+
+    render(){
+        console.log(this.props);
+        return(
+            <div className="container text-center">
+                <br/>
+                    <h2>Looks delicious isn't it?</h2>
+                <hr/>
+                <br/>
+                <div className="card col-lg-6 mx-auto">
+                    <img className="card-img-top active-recipe__img " src={this.props.location.state.recipe_img} alt="Card image cap"/>
+                    <div className="card-body">
+                        <h4 className="card-text">{this.props.location.state.recipe}</h4>
+                        <p className="lead"><b>Publisher: </b>{this.props.location.state.publisher}</p>
+                        <p className="lead"><b>Social Rank: </b>{Math.floor(this.props.location.state.rank)}</p>
+                        <p className="lead"><b>Source: </b>{this.props.location.state.source}</p>
+                    </div>
+                    
+                </div>
+                <br/>
+            </div>
+            
+        );
+    }
+}
+
+export default Recipe;
